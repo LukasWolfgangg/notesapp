e87c08f29eb1e8d1d2da01ec146e449152ae958d
Lukas Wolfgang <lukas.wolfgang@icloud.com>
2025-02-24 22:27:34 +0100

    Test

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Board from './components/Board';
+
+const PLAYER_1 = 'X';
+const PLAYER_2 = 'O';
+
+const generateSquares = () => {
+  const squares = [];
+
+  let currentId = 0;
+
+  for (let row = 0; row < 3; row += 1) {
+    squares.push([]);
+    for (let col = 0; col < 3; col += 1) {
+      squares[row].push({
+        id: currentId,
+        value: '',
+      });
+      currentId += 1;
+    }
+  }
+
+  return squares;
+};
+
+const App = () => {
+  // This starts state off as a 2D array of JS objects with
+  // empty value and unique ids.
+  const [squares, setSquares] = useState(generateSquares());
+
+  // Wave 2
+  // You will need to create a method to change the square
+  //   When it is clicked on.
+  //   Then pass it into the squares as a callback
+
+  const checkForWinner = () => {
+    // Complete in Wave 3
+    // You will need to:
+    // 1. Go accross each row to see if
+    //    3 squares in the same row match
+    //    i.e. same value
+    // 2. Go down each column to see if
+    //    3 squares in each column match
+    // 3. Go across each diagonal to see if
+    //    all three squares have the same value.
+  };
+
+  const resetGame = () => {
+    // Complete in Wave 4
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <header className="App-header">
+        <h1>React Tic Tac Toe</h1>
+        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <button>Reset Game</button>
+      </header>
+      <main>
+        <Board squares={squares} />
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
